856a6035ca3b2a50f6deed0cd70ee9b6ec360c4c
zfy1355 <zfy1355@163.com>
2016-07-22 23:34:02 +0800

    modify AndroidManifest.xml

--- app/src/main/java/com/example/home/myapplication/activity/HomeActivity.java
+++ app/src/main/java/com/example/home/myapplication/activity/HomeActivity.java
@@ -3,5 +3,19 @@ package com.example.home.myapplication.activity;
 /**
  * Created by home on 2016/7/22.
  */
-public class HomeActivity {
+public class HomeActivity extends BaseWapperActivity {
+    @Override
+    protected void loadViewLayout() {
+
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
 }
